55bd57c2ac873e54a8e8fbb5eeccc4e972e1d4ba
Carlos Soto <sotochappecarlos@gmail.com>
2022-11-02 14:27:47 -0500

    Se avanzo con la vista de create blade

--- resources/views/articulo/create.blade.php
+++ resources/views/articulo/create.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.plantillabase');
 
 @section('contenido')
-
+<h2>CREAR REGISTROS</h2>
+<form action="/articulos" method="POST">
+    <div class="mb-3">
+        <label for="" class="form-label">Código</label>
+        <input id="codigo" name="codigo" type="text" class="form-control" tabindex="1">
+    </div>
+    <div class="mb-3">
+        <label for="" class="form-label">Descripción</label>
+        <input id="decripcion" name="descripcion" type="text" class="form-control" tabindex="2">
+    </div>
+    <div class="mb-3">
+        <label for="" class="form-label">Cantidad</label>
+        <input id="cantidad" name="cantidad" type="number" class="form-control" tabindex="3">
+    </div>
+    <div class="mb-3">
+        <label for="" class="form-label">Precio</label>
+        <input id="precio" name="precio" type="number" step="any" value="0.00" class="form-control" tabindex="4">
+    </div>
+    <a href="/artocilos" class="btn btn-secondary" tabindex="5">Cancelar</a>
+    <button type="submit" class="btn btn-primary" tabindex="4">Guardar</button>
+</form>
 @endcontenido
--- resources/views/articulo/index.blade.php
+++ resources/views/articulo/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.plantillabase');
 
 @section('contenido')
-<a href="articulos/create" class="btn btn-primary"></a>
+<a href="articulos/create" class="btn btn-primary">CREAR</a>
 <table class="table table-dark table-striped mt-4">
     <thead>
         <tr>
