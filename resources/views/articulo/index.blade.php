29ef975295e442008bc8110dba3b36d96c5d4833
Carlos Soto <sotochappecarlos@gmail.com>
2022-11-02 13:53:39 -0500

    Se avanzo con la tabla de la vista index

--- resources/views/articulo/index.blade.php
+++ resources/views/articulo/index.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts.plantillabase');
 
 @section('contenido')
-<h1>Vista Index</h1>
+<a href="articulos/create" class="btn btn-primary"></a>
+<table class="table table-dark table-striped mt-4">
+    <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Código</th>
+            <th scope="col">Descripción</th>
+            <th scope="col">Cantidad</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($articulos as $articulo)
+        <tr>
+            <td>{{$articulo->id}}</td>
+            <td>{{$articulo->codigo}}</td>
+            <td>{{$articulo->descripcion}}</td>
+            <td>{{$articulo-cantidad}}</td>
+            <td>{{$articulo-precio}}</td>
+            <td>
+                <a href="" class="btn btn-info">Editar</a>
+                <button class="btn btn-danger">Borrar</button>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 @endsection
