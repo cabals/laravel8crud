3b91b9cbc2d0db8967fbf1c392d380941e6e2951
Carlos Soto <sotochappecarlos@gmail.com>
2022-11-02 15:24:12 -0500

    Se hizo las funciones de actualizar del controlador Articulo

--- app/Http/Controllers/ArticuloController.php
+++ app/Http/Controllers/ArticuloController.php
@@ -37,6 +37,7 @@ class ArticuloController extends Controller
     public function store(Request $request)
     {
         $articulos = new Articulo();
+        
         $articulos->codigo=$request->get('codigo');
         $articulos->descripcion=$request->get('descripcion');
         $articulos->cantidad=$request->get('cantidad');
@@ -66,7 +67,8 @@ class ArticuloController extends Controller
      */
     public function edit($id)
     {
-        //
+        $articulo = Articulo::find($id);
+        return view('articulo.edit')->with('articulo',$articulo);
     }
 
     /**
@@ -78,7 +80,15 @@ class ArticuloController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $articulo = Articulo::find($id);
+
+        $articulo->codigo=$request->get('codigo');
+        $articulo->descripcion=$request->get('descripcion');
+        $articulo->cantidad=$request->get('cantidad');
+        $articulo->precio=$request->get('precio');
+
+        $articulo->save();
+        return redirect('/articulos');
     }
 
     /**
