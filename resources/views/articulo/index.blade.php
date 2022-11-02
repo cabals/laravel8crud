b87a4499bb94e6e14cafb2951054753f943c5485
Carlos Soto <sotochappecarlos@gmail.com>
2022-11-02 15:31:35 -0500

    Se hizo la funcion destroy o borrar en el controlador Articulo

--- resources/views/articulo/index.blade.php
+++ resources/views/articulo/index.blade.php
@@ -22,8 +22,12 @@
             <td>{{$articulo->cantidad}}</td>
             <td>{{$articulo->precio}}</td>
             <td>
-                <a href="/articulos/{{$articulo->id}}/edit" class="btn btn-info">Editar</a>
-                <button class="btn btn-danger">Borrar</button>
+                <form action="{{route('articulos.destroy',$articulo->id)}}" method="POST">
+                    <a href="/articulos/{{$articulo->id}}/edit" class="btn btn-info">Editar</a>
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger">Borrar</button>
+                </form>
             </td>
         </tr>
         @endforeach
